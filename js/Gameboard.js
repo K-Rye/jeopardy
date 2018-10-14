78c87c9e25cfb9c99cfe94ec3a205fc8a82091b7
Kevin Ryan <kevinpatryan@gmail.com>
2018-10-14 17:07:36 -0600

    Generated gameboard categories and questions

--- js/Gameboard.js
+++ js/Gameboard.js
@@ -1,55 +1,43 @@
-class Gameboard {
-  constructor() {
-    this.possibleCategories = this.generateCategories();
-  }
+  class Gameboard {
+    constructor() {
+      this.possibleCategories = this.generateCategories();
+    }
 
     generateCategories(){
       let questionCategories = Object.keys(data.categories);
       return questionCategories
-  }
-
-    generateQuestions(currentRound) {
-      const gameboard = {
-        //need to iterate over clues array and radomly choose clues that have the same categoryId as the category in the questionCategories array and push into category 1-4.
-        category1: [],
-        category2: [],
-        category3: [],
-        category4: [],
-      //will generate categories and will generate questions from those categories.
-
-    }
-    //return gameboard;
-  }
-  
-
-    generateBoard(data) {
-    const board = {};
-    
-    //grab all 10 categories
-    const categories = Object.keys(data.categories);
-
-    //remove one at random until 5 are left
-    while (categories.length > 5) {
-      const randomCategory = categories.random();
-      categories.splice(categories.findIndexOf(randomCategory), 1);
     }
 
-    categories.forEach((category) => {
-      const categoryId = data.categories[category];
-      board[category] = [];
+    generateBoard(currentRound) {
 
-      for (let i = 0; i < data.clues.length; i++) {
-        if (data.clues[i].categoryId === categoryId) {
-          board[category].push(data.clues[i]);
-        }
-        if (board[category].length === 4) {
-          break;
-        }
+      const generateRandomIndex = (length) => {    
+        return Math.floor(Math.random() * length)
       }
-    })
 
-    return board;
-  }
+      let categories = Object.keys(data.categories)
+      let gameBoard = categories.map((category) => {
+        let myObject = {}
+        myObject.category = category
 
 
-}
+        let categoryId = data.categories[category] 
+
+
+        for (let i = 1; i < 6; i++) {
+
+          const possibleQuestions = data.clues.filter(clue => {
+            return clue.pointValue === i * 100 && clue.categoryId === categoryId
+          })
+
+          let question = possibleQuestions[generateRandomIndex(possibleQuestions.length)]
+          myObject[i * 100] = question
+        }
+
+        return myObject
+
+      })
+
+     return gameBoard; 
+
+
+    }
