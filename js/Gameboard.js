d6cdd003ac2c8e93c77dab00004410c9ad300197
Kevin Ryan <kevinpatryan@gmail.com>
2018-10-15 15:44:36 -0600

    Refactored gameboard created shuffle function and increase score for each round

--- js/Gameboard.js
+++ js/Gameboard.js
@@ -3,41 +3,52 @@
       this.possibleCategories = this.generateCategories();
     }
 
-    generateCategories(){
-      let questionCategories = Object.keys(data.categories);
-      return questionCategories
+  
+    const generateRandomIndex = (length) => {    
+      return Math.floor(Math.random() * length)
     }
 
-    generateBoard(currentRound) {
-
-      const generateRandomIndex = (length) => {    
-        return Math.floor(Math.random() * length)
-      }
-
-      let categories = Object.keys(data.categories)
-      let gameBoard = categories.map((category) => {
-        let myObject = {}
-        myObject.category = category
-
-
-        let categoryId = data.categories[category] 
-
-
-        for (let i = 1; i < 6; i++) {
-
-          const possibleQuestions = data.clues.filter(clue => {
-            return clue.pointValue === i * 100 && clue.categoryId === categoryId
-          })
-
-          let question = possibleQuestions[generateRandomIndex(possibleQuestions.length)]
-          myObject[i * 100] = question
-        }
-
-        return myObject
-
+    const chooseQuestion = (pointValue, categoryId, categoryIndex) => {
+    
+      const possibleQuestions = data.clues.filter(clue => {
+        return clue.pointValue === pointValue && clue.categoryId === categoryId
       })
 
-     return gameBoard; 
+      const randomQuestion = possibleQuestions[generateRandomIndex(possibleQuestions.length)]
 
+      randomQuestion.pointValue *= (Math.floor((categoryIndex) / 4) + 1)
 
+      return randomQuestion;
     }
+
+    // copied from stack overflow
+    const shuffle = (a) => {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    }
+
+    // shuffle through the categories and choose randomly when invoked
+    let categories = Object.keys(data.categories)
+    categories = shuffle(categories)
+
+    let gameBoard = categories.map((category, categoryIndex) => {
+      let gb_categoryObj = {}
+      gb_categoryObj.category = category
+
+      let categoryId = data.categories[category] 
+
+        // 4. we are looping over 5 times to polulate our category and increase the key value by 100 each time  
+        for (let i = 1; i < 6; i++) {
+          // 3. we set the value of our key which is a point value to the question that was randomly choosen in our chooseQuestion function
+          gb_categoryObj[i * 100] = chooseQuestion(i * 100, categoryId, categoryIndex);
+        }
+
+      return gb_categoryObj
+    })
+
+  return gameBoard; 
+
+}
