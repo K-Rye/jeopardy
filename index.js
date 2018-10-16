a866664ed219664eb791b88ba224a034cf0d1a2b
chris023 <chris.meyer023@gmail.com>
2018-10-16 16:11:05 -0600

    Removed unusued code

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-
 const game = new Game();
 game.promptForPlayerNames();
 game.start(Gameboard.generateGameboard());
 game.init();
-
-
-DomUtilities.clickedOnGameboard();
-
-const domUtilities = new DomUtilities();
-domUtilities.clickedOnGameboard();
