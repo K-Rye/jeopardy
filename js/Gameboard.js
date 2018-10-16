fd30d83b314b4bf44d382836bc9f1ccfaa1c825d
chris023 <chris.meyer023@gmail.com>
2018-10-16 09:49:47 -0600

    Moved helper methods out of gameboard

--- js/Gameboard.js
+++ js/Gameboard.js
@@ -1,52 +1,30 @@
-  class Gameboard {
-    constructor() {
-      this.possibleCategories = this.generateCategories();
-    }
+class Gameboard {
 
-  
-    const generateRandomIndex = (length) => {    
-      return Math.floor(Math.random() * length)
-    }
+  constructor() {
+    this.possibleCategories = this.generateCategories();
+  }
 
-    const chooseQuestion = (pointValue, categoryId, categoryIndex) => {
-    
-      const possibleQuestions = data.clues.filter(clue => {
-        return clue.pointValue === pointValue && clue.categoryId === categoryId
-      })
-
-      const randomQuestion = possibleQuestions[generateRandomIndex(possibleQuestions.length)]
-
-      randomQuestion.pointValue *= (Math.floor((categoryIndex) / 4) + 1)
-
-      return randomQuestion;
-    }
-
-    const shuffle = (a) => {
-      for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-      }
-      return a;
-    }
+  generateGameboard() {
 
     let categories = Object.keys(data.categories)
-    categories = shuffle(categories)
+    categories = Utilities.shuffle(categories)
 
     let gameBoard = categories.map((category, categoryIndex) => {
       let gb_categoryObj = {}
       gb_categoryObj.category = category
 
-      let categoryId = data.categories[category] 
+      let categoryId = data.categories[category]
 
-      
-        for (let i = 1; i < 6; i++) {
-          
-          gb_categoryObj[i * 100] = chooseQuestion(i * 100, categoryId, categoryIndex);
-        }
+    
+      for (let i = 1; i < 6; i++) {
+        
+        gb_categoryObj[i * 100] = Utilities.chooseQuestion(i * 100, categoryId, categoryIndex);
+      }
 
       return gb_categoryObj
     })
 
-  return gameBoard; 
+    return gameBoard;
+  }
 
 }
