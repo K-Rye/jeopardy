12fe2ba941876acc01cc9c3928b5f6e72fbea81b
Kevin Ryan <kevinpatryan@gmail.com>
2018-10-17 14:52:45 -0600

    Add dailydouble function to randomly choose section on gameboard to place daily double

--- js/Game.js
+++ js/Game.js
@@ -6,6 +6,7 @@ class Game  {
     this.playerTurn = 0;
     this.board = null;
     this.currentQuestion;
+    this.dailydouble = this.generateDailyDoubleLocation();
   }
 
   init() {
@@ -35,6 +36,35 @@ class Game  {
   promptForPlayerNames() {
     $('.player-name-popup').removeClass('hide');
   }
+
+  dailyDoubleWager() {
+    //this needs to pop up when the daily double is clicked
+    //it will display the wager window which will have an input and submit button
+    //when the wager is entered and validated, the question will display with an input
+    //and submit button 
+
+    this.currentQuestion = this.board[categoryNumber - 1][questionLevel];
+
+    $('.dailyDouble-popup-wager').removeClass('hide');
+    $('.dailyDouble-popup-wager-submission').text(this.currentQuestion.question);
+    }
+
+  dailyDoubleWagerWindow() {
+    $('.dailyDouble-popup-wager').removeClass('hide');
+  }
+  
+  
+  generateDailyDoubleLocation() {
+    const dailydoubleLocation = {}
+    dailydoubleLocation.category = Math.floor(Math.random() * 4) + 1 
+    dailydoubleLocation.pointValue = (Math.floor(Math.random() * 5) + 1) *100
+
+    return dailydoubleLocation
+  }
+
+
+
+
 
   declareWinner() {
     //
